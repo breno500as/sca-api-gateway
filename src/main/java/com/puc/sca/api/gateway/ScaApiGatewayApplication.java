0bf72a079cc5b14b7d0ed71b27fcbcf2ada36950
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-07-04 15:10:53 -0300

    configurações iniciais

---
+++ src/main/java/com/puc/sca/api/gateway/ScaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.puc.sca.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaApiGatewayApplication.class, args);
+	}
+
+}
