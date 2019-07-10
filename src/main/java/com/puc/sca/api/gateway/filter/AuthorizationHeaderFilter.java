b6818a7bc49151b24d02b43fa3462e37680a13ab
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-07-10 14:53:04 -0300

    propagação de usuário

--- src/main/java/com/puc/sca/api/gateway/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/puc/sca/api/gateway/filter/AuthorizationHeaderFilter.java
@@ -2,7 +2,12 @@ package com.puc.sca.api.gateway.filter;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.core.Ordered;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -29,12 +34,28 @@ public class AuthorizationHeaderFilter extends ZuulFilter {
 		
 		if (authentication != null) {
 			UsernamePasswordAuthenticationToken u = (UsernamePasswordAuthenticationToken) authentication;
-			Usuario usuario = (Usuario) u.getPrincipal();
-			Collection<SimpleGrantedAuthority> autoridades = (Collection<SimpleGrantedAuthority>) authentication.getAuthorities();
-			String authorizationHeader = (String) u.getCredentials();
-		    ctx.addZuulRequestHeader("Authorization", AcessoSeguroFilter.BEARER + " " + authorizationHeader);
+		 	Usuario usuario = (Usuario) u.getPrincipal();
+  
+	
+   		    // Uma forma de passar o token via header para um outro microserviço
+		    ctx.addZuulRequestHeader("Authorization", AcessoSeguroFilter.BEARER + " " + (String) u.getCredentials());
 			ctx.set("idUsuario", usuario.getId());
-
+	
+			
+			Map<String, List<String>> newParameterMap = new HashMap<String, List<String>>();
+			
+			// Uma forma de passar o id do usuário e as permissões para um outro microserviço;
+			newParameterMap.put("idUsuario", Arrays.asList(usuario.getId().toString()));
+			
+			Collection<SimpleGrantedAuthority> permissoes = (Collection<SimpleGrantedAuthority>) authentication.getAuthorities();
+			if (permissoes != null) {
+				String list = permissoes.stream().map(a -> a.getAuthority()).collect(Collectors.joining(","));
+				newParameterMap.put("permissoes", Arrays.asList(list));
+			}
+			
+		
+			ctx.setRequestQueryParams(newParameterMap);
+		 
 		}
 		
 	
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 insert into Usuario (id, nome, senha, email) values  (1, 'breno500as', '64E5E2DB1CB10783AE977319C188D8DB', 'breno500as@gmail.com');
 insert into Permissao (id, descricao, usuario_id) values  (1, 'ADMIN', 1);
+insert into Permissao (id, descricao, usuario_id) values  (2, 'TESTE', 1);
