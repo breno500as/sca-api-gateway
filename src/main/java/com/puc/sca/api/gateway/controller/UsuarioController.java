da379730690aeb3af2ebf799e8eaf3853554bab2
breno.mcosta <breno.mcosta@pbh.gov.br>
2019-08-15 20:16:22 -0300

    eureka

--- src/main/java/com/puc/sca/api/gateway/controller/UsuarioController.java
+++ src/main/java/com/puc/sca/api/gateway/controller/UsuarioController.java
@@ -2,6 +2,7 @@ package com.puc.sca.api.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,11 @@ public class UsuarioController {
 	@GetMapping
 	public Iterable<Usuario> recuperaUsuarios() {
 		return this.usuarioRepository.findAll();
+	}
+	
+	@GetMapping("{id}")
+	public Usuario findById(@PathVariable(value = "id")  Long id) {
+	  return this.usuarioRepository.findById(id).get();	
 	}
 
 }
