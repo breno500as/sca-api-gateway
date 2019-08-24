a128d21fe08fed8868163c6f493f486204bfda71
breno <breno@breno-Inspiron-7580>
2019-08-23 23:47:51 -0300

    circuit breaker

--- src/main/java/com/puc/sca/api/gateway/controller/UsuarioController.java
+++ src/main/java/com/puc/sca/api/gateway/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.puc.sca.api.gateway.controller;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.puc.sca.api.gateway.entity.Usuario;
 import com.puc.sca.api.gateway.repository.UsuarioRepository;
 
@@ -17,13 +20,18 @@ public class UsuarioController {
 	private UsuarioRepository usuarioRepository;
 
 	@GetMapping
+	@HystrixCommand(fallbackMethod = "reliableUsers")
 	public Iterable<Usuario> recuperaUsuarios() {
 		return this.usuarioRepository.findAll();
 	}
-	
+
+	public Iterable<Usuario> reliableUsers() {
+		return Arrays.asList(new Usuario(1L, "BRENO PEREIRA"));
+	}
+
 	@GetMapping("{id}")
-	public Usuario findById(@PathVariable(value = "id")  Long id) {
-	  return this.usuarioRepository.findById(id).get();	
+	public Usuario findById(@PathVariable(value = "id") Long id) {
+		return this.usuarioRepository.findById(id).get();
 	}
 
 }
--- src/main/java/com/puc/sca/api/gateway/entity/Usuario.java
+++ src/main/java/com/puc/sca/api/gateway/entity/Usuario.java
@@ -42,6 +42,14 @@ public class Usuario implements Serializable {
 	@Transient
 	private String token;
 
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
 	public Long getId() {
 		return id;
 	}
