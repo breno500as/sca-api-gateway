17c443186afcbb11d54d241988bc5124c6aef707
breno500as <breno500as@github.com>
2019-08-28 23:35:04 -0300

    heroku

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,8 @@
-insert into Usuario (id, nome, senha, email) values  (1, 'BRENO PEREIRA', 'cd902b80e7cb42145148b41acd603042', 'admin@gmail.com');
-insert into Usuario (id, nome, senha, email) values  (2, 'CARLOS EDUARDO', '745669e31d39ccc72ea98a3f911757a7', 'carlos.eduardo@uol.com.br');
-insert into Usuario (id, nome, senha, email) values  (3, 'MARISA MONTE', 'a50729da6d06af8d1ad151c8c6ee5bd4', 'mmonte@yahoo.com');
-insert into Usuario (id, nome, senha, email) values  (4, 'TIAGO SILVA', 'e32e51070f3b190082423cef7fb3de28', 'tiagosilva1985@gmail.com');
+insert into Usuario (id, nome, senha, email) values  (1, 'ADMIN', 'cd902b80e7cb42145148b41acd603042', 'admin@gmail.com');
+insert into Usuario (id, nome, senha, email) values  (2, 'BRENO PEREIRA', 'c84ff789436a5d7f0bf8504bb6b69d83', 'teste@gmail.com');
+insert into Usuario (id, nome, senha, email) values  (3, 'MARISA MONTE', 'eacd672d120794985928af70c63da849', 'mmonte@yahoo.com');
+insert into Usuario (id, nome, senha, email) values  (4, 'TIAGO SILVA', 'c79924ec82f57b59c26cf5c1e2b8b6ca', 'tiagosilva1985@gmail.com');
+insert into Usuario (id, nome, senha, email) values  (5, 'NOVAK DJOKOVIC', 'b2375cf689675824504b2df74434037e', 'novak@gmail.com');
 insert into Permissao (id, descricao, usuario_id) values  (1, 'ADMIN', 1);
 insert into Permissao (id, descricao, usuario_id) values  (2, 'OPERADOR', 2);
 insert into Permissao (id, descricao, usuario_id) values  (3, 'TERCEIRIZADO', 3);
