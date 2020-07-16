be956c08b6a26cedd04d812488522f811ca88df9
breno500as <breno500as@gmail.com>
2020-07-16 11:52:54 -0300

    log

--- src/main/java/com/puc/sca/api/gateway/exception/InvalidTokenJwtException.java
+++ src/main/java/com/puc/sca/api/gateway/exception/InvalidTokenJwtException.java
@@ -3,7 +3,11 @@ package com.puc.sca.api.gateway.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+/**
+ * Usuário não autorizado.
+ * @author breno
+ *
+ */
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidTokenJwtException extends AuthenticationException {
 
